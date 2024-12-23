bb651c5208a9ea7146928314f6b46f60e655eb03
Richard Huss <richard@huss.org.uk>
2024-12-23 13:58:48 +0000

    Rename

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { app, ipcMain } from 'electron'
 import setAboutPanelOptions from './about.js'
 import openSettingsWindow from './settingsWindow.js'
 import createTray from './tray.js'
-import addIpcHandlers from './ipc.js'
+import addIpcHandlers from './ipcHandlers.js'
 
 app.whenReady().then(() => {
 

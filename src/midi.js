e2f66dbd9b5989b942ac5c735f004d026577c498
Richard Huss <richard@huss.org.uk>
2024-12-24 21:23:16 +0000

    Actually press Companion buttons :-)

--- src/midi.js
+++ src/midi.js
@@ -30,10 +30,14 @@ export function startListening() {
     const midiMessageChannel = message[0] & 0x0f
     const midiMessageNote = message[1]
     const midiMessageVelocity = message[2]
+
     console.log(
-      'debug',
       `MIDI Message: Midi Channel: ${midiMessageChannel}, Is Note On?: ${midiMessageIsNoteon}, Note: ${midiMessageNote}, Velocity: ${midiMessageVelocity}, Delta Time: ${deltaTime}`,
     )
+    if (midiMessageIsNoteon) {
+      pressCompanionButton(midiMessageChannel, midiMessageNote, midiMessageVelocity)
+    }
+
   })
 }
 
@@ -44,4 +48,28 @@ export function stopListening() {
     midi_input.destroy()
     midi_input = new Input()
   }
+}
+
+function pressCompanionButton(page, row, column) {
+  const settings = getSettings()
+  // api/location/<page>/<row>/<column>/press
+  // page = channel
+  // row = note
+  // column = velocity
+  const buttonPressURL = `http://${settings.companionHost}:${settings.companionPort}/api/location/${page + 1 + settings.pageOffset}/${row}/${column}/press`
+  console.log('Sending button press HTTP request to: ' + buttonPressURL)
+
+  fetch(buttonPressURL, {
+    signal: AbortSignal.timeout(2000),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then((response) => {
+    console.log('Button press response: ' + response.status + ': ' + response.statusText)
+  })
+  .catch((error) => {
+    console.log('Error fetching ' + buttonPressURL + '. ' + error)
+  })
 }

be9d204f160c83650b98ac6369fa764712c8cff6
Richard Huss <richard@huss.org.uk>
2024-12-25 14:13:37 +0000

    Start using electron-forge

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import addIpcHandlers from './ipcHandlers.js'
 import { loadSettings, isConfigured } from './settings.js'
 import { startListening } from './midi.js'
 
+import started from 'electron-squirrel-startup'
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (started) {
+  app.quit()
+}
+
 app.whenReady().then(() => {
   loadSettings()
   addIpcHandlers()

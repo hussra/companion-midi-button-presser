a23d1bca904658cd992257bbac4c0a98f4b73432
Richard Huss <richard@huss.org.uk>
2024-12-25 22:51:56 +0000

    Hostname or IP address

--- src/settings.js
+++ src/settings.js
@@ -5,7 +5,7 @@ let settings = null
 const schema = {
   companionHost: {
     type: 'string',
-    format: 'ipv4',
+    pattern: '(^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$)',
     default: '127.0.0.1'
   },
   companionPort: {

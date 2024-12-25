7b09958e86ebd0ab19e9f2d156b5e1e35286bac2
Richard Huss <richard@huss.org.uk>
2024-12-25 14:26:00 +0000

    Fix tray icon in packaged app

--- src/tray.js
+++ src/tray.js
@@ -1,4 +1,5 @@
 import { app, Tray, Menu, nativeImage } from 'electron'
+import path from 'path'
 
 import openSettingsWindow from './settingsWindow.js'
 import pkg from '../package.json' with { type: 'json' }
@@ -7,7 +8,9 @@ let tray
 
 export default function createTray() {
 
-  const icon = nativeImage.createFromPath('assets/Black_question_mark.png')
+  const assetsPath = app.isPackaged ? path.join(process.resourcesPath, "app", "assets") : "assets";
+  const icon = nativeImage.createFromPath(path.join(assetsPath, 'Black_question_mark.png'))
+  
   tray = new Tray(icon)
 
   const contextMenu = Menu.buildFromTemplate(
